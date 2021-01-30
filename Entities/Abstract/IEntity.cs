8b320119bef57227042521643fc5755a072324a6
Buse Akyüz <buse.akyuz29@gmail.com>
2021-01-31 00:20:47 +0300

    Proje dosyası ekle.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    // Ientity implement eden class bir veritabanı tablosudur-ED
+   public interface IEntity
+    {
+
+    }
+}
